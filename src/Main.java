7706f0048a6ebe40b7a292e7b0453781ee30b3e9
Hector <hectormg.gu15@icloud.com>
2026-06-12 11:28:35 -0600

    Constructor doesn't need a value, it is added with the addNode method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        BinaryTree new_tree = new BinaryTree(6);
+        BinaryTree new_tree = new BinaryTree();
+        new_tree.addNode(6);
         new_tree.addNode(7);
         new_tree.addNode(5);
         new_tree.addNode(4);
